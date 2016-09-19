37e6108e44cd77c182a7643a0cabedf738a587f4
marcu_000 <marcu_000@RockyLaptop>
2016-09-19 14:47:45 -0500

    ++ before x

--- RPG-Game/src/code/Map.java
+++ RPG-Game/src/code/Map.java
@@ -8,11 +8,11 @@ public Map(int xSize, int ySize){
 }
 private Object[][] size = new Object[xMap][yMap];
 public void sqrSides(){
-	for (int x=0; x<xMap; x++){ 
+	for (int x=0; x<xMap; ++x){ 
 		size[x][0]= new Wall();
 		size[x][yMap]= new Wall();
 	}
-	for (int y=0; y<yMap; y++){
+	for (int y=0; y<yMap; ++y){
 		size[0][y]= new Wall();
 		size[xMap][y]= new Wall();
 	}
